09c6bcf16091533cab205431430b28385356dff2
Brian Mlambo <bmwakondo@gmail.com>
2026-04-21 09:29:39 +0300

    add …

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -2,7 +2,7 @@ import"../styles/navbar.css"
 export default function Navbar(){
     return(
         <nav className="navbar">
-            <h1>NextGen Solutions</h1>
+            <h1 className="logo">NextGen Solutions</h1>
             <ul className="nav-links">
                 <li> <a href="#home">Home</a></li>
                 <li> <a href="#about">About</a></li>
