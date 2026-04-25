1e41617e9005b6c0b5092a1a2660988a055862ea
Brian Mlambo <bmwakondo@gmail.com>
2026-04-25 09:18:39 +0300

    add …

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -1,14 +1,39 @@
 import "../styles/contact.css"
+import { useState } from "react"
 export default function Contact(){
+    const[name,setName]=useState("")
+    const[email,setEmail]=useState("")
+    const[message,setMessage]=useState("")
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        alert("Form submitted")
+        setName("")
+        setEmail("")
+        setMessage("")
+    }
     return(
         <section id="contact">
             <div className="contact-container">
                 <h2>Get in touch</h2>
                 <p>We'd love to here from you.Fill out the form below and will get back to you.</p>
-                <form>
-                    <input type="text" placeholder="your name" />
-                    <input type="email" placeholder="your email" />
-                    <textarea placeholder="your message"></textarea>
+                <form onSubmit={handleSubmit}>
+                    <input type="text"
+                     placeholder="your name"
+                     value={name}
+                     onChange={(e)=>setName(e.target.value)}
+
+                     />
+                    <input type="email"
+                     placeholder="your email"
+                     value={email}
+                     onChange={(e)=>setEmail(e.target.value)}
+                     
+                     />
+                    <textarea placeholder="your message"
+                    value={message}
+                    onChange={(e)=>setMessage(e.target.value)}
+                    
+                    ></textarea>
                     <button type="submit">Send message</button>
                 </form>
             </div>
